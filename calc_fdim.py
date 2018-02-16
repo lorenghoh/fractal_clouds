89653d091c50864e5e2bf99a8311eb30d3b7a89c
Loren Oh <loren.gh.oh@gmail.com>
2018-02-16 15:04:57 -0800

    Bugfix for sub-domain mapping adjustment

--- calc_fdim.py
+++ calc_fdim.py
@@ -6,23 +6,56 @@ import pyarrow.parquet as pq
 def count_box():
     pass
 
-def index_to_zyx(index):
-    z = index // (ny * nx)
-    xy = index % (ny * nx)
-    y = xy // nx
-    x = xy % nx
-    return pd.DataFrame({'z':z, 'y':y, 'x':x})
-
 if __name__ == '__main__':
     # Calculate fdim from a sample cloud 
     path = '/nodessd/loh/temp'
     df = pq.read_table(f'{path}/clouds.pq', nthreads=6).to_pandas()
-    df = df[(df.cid == 11023) & (df.type == 4)]
+    df = df[(df.cid == 6886) & (df.type == 4)]
 
-    # Calculate zyx indices from coordinates
-    xy = df.coord % (256 * 256)
+    # Calculate z index from coordinates
     df['z'] = df.coord // (256 * 256)
+
+    k_sample = df.z.value_counts().index[0]
+    df = df[(df.z == k_sample)]
+
+    # From there, take the xy indices
+    xy = df.coord % (256 * 256)
     df['y'] = xy // 256 
     df['x'] = xy % 256
 
-    print(df[(df.z > 30) & (df.z < 40)].sort_values(by=['z']))
+    x = df.x.values
+    y = df.y.values
+
+    print(df.head())
+
+    # Map cloud core onto the BOMEX domain and adjust
+    xy_map = np.zeros((256, 256), dtype=int)
+    xy_map[y, x] = 1
+
+    x_axis, y_axis = xy_map.shape
+    if (max(x) - min(x)) > x_axis // 2:
+        # Shift target array
+        x_off = x_axis - min(x[(x > x_axis // 2)])
+        xy_map = np.roll(xy_map, x_off, axis=1)
+
+        # Shift x-coordinates
+        x = x + x_off
+        x[x >= x_axis] = x[x >= x_axis] - x_axis
+
+    if (max(y) - min(y)) > y_axis // 2:
+        # Shift target array
+        y_off = y_axis - min(y[(y > y_axis // 2)])
+        xy_map = np.roll(xy_map, y_off, axis=0)
+        
+        # Shift y-coordinates
+        y = y + y_off
+        y[y >= y_axis] = y[y >= y_axis] - y_axis
+
+    x_width = max(x) - min(x)
+    y_width = max(y) - min(y)
+    print(f"\nCorresponding sub-domain size: {y_width}x{x_width}")
+    print( "Adjusted coordinates: " \
+          f"({min(y)}, {max(y)}), ({min(x)}, {max(x)})")
+
+    # Then map it onto a new 2D array
+    print(xy_map[min(y):max(y), min(x):max(x)])
