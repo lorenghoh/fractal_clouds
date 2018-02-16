4afc488302f709b252f59b3f1c7d6366ab93601b
Loren Oh <loren.gh.oh@gmail.com>
2018-02-16 15:48:46 -0800

    Draw a contour plot of a projection

---
+++ plot_observed_cloud.py
@@ -0,0 +1,60 @@
+import numpy as np
+import numba as nb
+import pandas as pd
+import pyarrow.parquet as pq 
+
+import matplotlib, os
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from pick_cloud_projection import pick_cid 
+
+if __name__ == '__main__':
+    # Calculate fdim from a sample cloud 
+    # Read horizontal slice from a cloud core
+    xy_map, x, y = pick_cid(6886, 4)
+
+    x_width = max(x) - min(x)
+    y_width = max(y) - min(y)
+    print(f"\nCorresponding sub-domain size: {y_width}x{x_width}")
+    print( "Adjusted coordinates: " \
+          f"({min(y)}, {max(y)}), ({min(x)}, {max(x)})")
+
+    # Map the projection onto a new 2D array
+    xy_map_sub = np.zeros((y_width+1, x_width+1), dtype=int)
+    x_sub = x - min(x)
+    y_sub = y - min(y)
+    xy_map_sub[y_sub, x_sub] = 1
+
+    print(xy_map_sub.shape)
+
+    #---- Plotting 
+    fig = plt.figure(1, figsize=(3, 3))
+    fig.clf()
+    sns.set_context('paper')
+    sns.set_style('ticks', 
+        {
+            'axes.grid': True, 
+            'axes.linewidth': '0.75',
+            'grid.color': '0.75',
+            'grid.linestyle': u':',
+            'legend.frameon': True,
+        })
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='Helvetica')
+
+    cmap = sns.cubehelix_palette(start=1.2, hue=1, \
+                                 light=1, rot=-1.05, as_cmap=True)
+
+    ax = plt.subplot(1, 1, 1)
+    xi = np.arange(max(x_sub)+1)
+    yi = np.arange(max(y_sub)+1)
+
+    im = plt.contour(xi, yi, xy_map_sub, cmap=cmap, lw=0.5)
+
+    plt.tight_layout(pad=0.5)
+    figfile = 'png/{}.png'.format(os.path.splitext(__file__)[0])
+    print('\t Writing figure to {}...'.format(figfile))
+    plt.savefig(figfile,bbox_inches='tight', dpi=180, \
+                facecolor='w', transparent=True)
