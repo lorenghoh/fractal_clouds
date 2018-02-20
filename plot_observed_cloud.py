0219beac1aea7670d08f0e6c8f5e949f896c014e
Loren Oh <loren.gh.oh@gmail.com>
2018-02-20 13:00:03 -0800

    Add +2 padding to plot

--- plot_observed_cloud.py
+++ plot_observed_cloud.py
@@ -13,7 +13,7 @@ from pick_cloud_projection import pick_cid
 if __name__ == '__main__':
     # Calculate fdim from a sample cloud 
     # Read horizontal slice from a cloud core
-    xy_map, x, y = pick_cid(6886, 0)
+    xy_map, x, y = pick_cid(7425, 4)
 
     x_width = max(x) - min(x)
     y_width = max(y) - min(y)
@@ -21,10 +21,10 @@ if __name__ == '__main__':
     print( "Adjusted coordinates: " \
           f"({min(y)}, {max(y)}), ({min(x)}, {max(x)})")
 
-    # Map the projection onto a new 2D array
-    xy_map_sub = np.zeros((y_width+1, x_width+1), dtype=int)
-    x_sub = x - min(x)
-    y_sub = y - min(y)
+    # Map the projection onto a new 2D array (+4 padding)
+    xy_map_sub = np.zeros((y_width+1+4, x_width+1+4), dtype=int)
+    x_sub = x - min(x) + 2
+    y_sub = y - min(y) + 2
     xy_map_sub[y_sub, x_sub] = 1
 
     print(xy_map_sub.shape)
@@ -48,8 +48,8 @@ if __name__ == '__main__':
                                  light=1, rot=-1.05, as_cmap=True)
 
     ax = plt.subplot(1, 1, 1)
-    xi = np.arange(max(x_sub)+1)
-    yi = np.arange(max(y_sub)+1)
+    xi = np.arange(x_width+1+4)
+    yi = np.arange(y_width+1+4)
 
     im = plt.pcolormesh(xi, yi, xy_map_sub, cmap=cmap, lw=0.5)
 
