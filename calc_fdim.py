a43b3080e977473078dab65dc26ec87f60094c0c
Loren Oh <loren.gh.oh@gmail.com>
2018-02-20 13:01:39 -0800

    Box counting for the perimeter

--- calc_fdim.py
+++ calc_fdim.py
@@ -3,21 +3,25 @@ import numba as nb
 import pandas as pd
 import pyarrow.parquet as pq 
 
+import matplotlib, os
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from pick_cloud_projection import pick_cid 
 
-def count_box(xy_map, k):
-    def boxcount(Z, k):
-        S = np.add.reduceat(
-            np.add.reduceat(Z, np.arange(0, Z.shape[0], k), axis=0),
-                               np.arange(0, Z.shape[1], k), axis=1)
+def count_box(Z, k):
+    S = np.add.reduceat(
+        np.add.reduceat(Z, np.arange(0, Z.shape[0], k), axis=0),
+                           np.arange(0, Z.shape[1], k), axis=1)
 
-        # We count non-empty (0) and non-full boxes (k*k)
-        return len(np.where((S > 0) & (S < k*k))[0])
+    # We count non-empty (0) and non-full boxes (k*k)
+    return len(np.where((S > 0) & (S < k*k))[0])
 
 if __name__ == '__main__':
     # Calculate fdim from a sample cloud 
     # Read horizontal slice from a cloud core
-    xy_map, x, y = pick_cid(6886, 4)
+    xy_map, x, y = pick_cid(11023, 0)
 
     x_width = max(x) - min(x)
     y_width = max(y) - min(y)
@@ -26,7 +30,64 @@ if __name__ == '__main__':
           f"({min(y)}, {max(y)}), ({min(x)}, {max(x)})")
 
     # Map the projection onto a new 2D array (2x size)
-    xy_map_sub = np.zeros((y_width*2, x_width*2), dtype=int)
+    xy_map_sub = np.zeros((y_width*3, x_width*3), dtype=int)
     x_sub = x - min(x) + x_width // 2
     y_sub = y - min(y) + y_width // 2
     xy_map_sub[y_sub, x_sub] = 1
+
+    # Leave only the perimeter of the cloud 
+    xy_temp = np.roll(xy_map_sub, 1, axis=0) \
+            + np.roll(xy_map_sub, -1, axis=0) \
+            + np.roll(xy_map_sub, 1, axis=1) \
+            + np.roll(xy_map_sub, -1, axis=1)
+    xy_map_sub[xy_temp == 4] = 0
+
+    # Minimal dimension of image
+    p = min(xy_map_sub.shape)
+
+    # Greatest power of 2 less than or equal to p
+    n = 2**np.floor(np.log(p)/np.log(2))
+
+    # Extract the exponent
+    n = int(np.log(n)/np.log(2))
+
+    # Build successive box sizes (from 2**n down to 2**1)
+    sizes = 2**np.arange(n, 1, -1)
+    # sizes = np.arange(p, 1, -1)
+
+    # Actual box counting with decreasing size
+    counts = []
+    for size in sizes:
+        counts.append(count_box(xy_map_sub, size))
+    
+    # Fit the successive log(sizes) with log (counts)
+    coeffs = np.polyfit(np.log(sizes), np.log(counts), 1)
+    print(-coeffs[0])
+
+    #---- Plotting 
+    fig = plt.figure(1, figsize=(3, 3))
+    fig.clf()
+    sns.set_context('paper')
+    sns.set_style('ticks', 
+        {
+            'axes.grid': True, 
+            'axes.linewidth': '0.75',
+            'grid.color': '0.75',
+            'grid.linestyle': u':',
+            'legend.frameon': True,
+        })
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='Helvetica')
+
+    cmap = sns.cubehelix_palette(start=1.2, hue=1, \
+                                 light=1, rot=-1.05, as_cmap=True)
+
+    ax = plt.subplot(1, 1, 1)
+
+    plt.plot(np.log(sizes), np.log(counts), '-o')
+
+    plt.tight_layout(pad=0.5)
+    figfile = 'png/{}.png'.format(os.path.splitext(__file__)[0])
+    print('\t Writing figure to {}...'.format(figfile))
+    plt.savefig(figfile,bbox_inches='tight', dpi=180, \
+                facecolor='w', transparent=True)
