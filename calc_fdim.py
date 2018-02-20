30f3c252a0c1716e0f717d6c7eb0d6bcd76e43b0
Loren Oh <loren.gh.oh@gmail.com>
2018-02-19 17:43:29 -0800

    Merge fdim-related scripts

--- calc_fdim.py
+++ calc_fdim.py
@@ -5,8 +5,14 @@ import pyarrow.parquet as pq
 
 from pick_cloud_projection import pick_cid 
 
-def count_box():
-    pass
+def count_box(xy_map, k):
+    def boxcount(Z, k):
+        S = np.add.reduceat(
+            np.add.reduceat(Z, np.arange(0, Z.shape[0], k), axis=0),
+                               np.arange(0, Z.shape[1], k), axis=1)
+
+        # We count non-empty (0) and non-full boxes (k*k)
+        return len(np.where((S > 0) & (S < k*k))[0])
 
 if __name__ == '__main__':
     # Calculate fdim from a sample cloud 
@@ -19,9 +25,8 @@ if __name__ == '__main__':
     print( "Adjusted coordinates: " \
           f"({min(y)}, {max(y)}), ({min(x)}, {max(x)})")
 
-    # Map the projection onto a new 2D array
-    xy_map_sub = np.zeros((x_width*2, y_width*2), dtype=int)
+    # Map the projection onto a new 2D array (2x size)
+    xy_map_sub = np.zeros((y_width*2, x_width*2), dtype=int)
     x_sub = x - min(x) + x_width // 2
     y_sub = y - min(y) + y_width // 2
     xy_map_sub[y_sub, x_sub] = 1
-
