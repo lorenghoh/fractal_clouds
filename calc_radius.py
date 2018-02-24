08a9f1394433de0c052700d0420fc6b3e8511ebb
Loren Oh <loren.gh.oh@gmail.com>
2018-02-24 15:44:26 -0800

    Calculate both geometric and radial distance

--- calc_radius.py
+++ calc_radius.py
@@ -5,14 +5,20 @@ import pyarrow.parquet as pq
 
 from pick_cloud_projection import pick_cid 
 
+# Return (x, y) tuple of COM point
 def calculate_com(df):
-    pass
+    return df.x.mean(), df.y.mean()
 
 def calculate_geometric_r(df):
     return np.sqrt(df.shape[0])
 
-def calculate_radial_r():
-    pass
+def calculate_radial_distance(df):
+    x_com, y_com = calculate_com(df)
+
+    del_x = df.x - x_com
+    del_y = df.y - y_com
+    
+    return np.mean(np.sqrt(del_x**2 + del_y**2))
 
 if __name__ == '__main__':
     # Calculate fdim from a sample cloud 
@@ -20,5 +26,11 @@ if __name__ == '__main__':
     df = pick_cid(4563, 0)
 
     # Geometric radius
+    r_g = calculate_geometric_r(df)
 
     # Average radial distance
+    r_d = calculate_radial_distance(df)
+
+    print(r_g)
+    print(r_d)
+    
