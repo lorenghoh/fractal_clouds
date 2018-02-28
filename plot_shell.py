d6e09cb25c29df139db2c3c4c9ef6fd68417b4b6
Loren Oh <loren.gh.oh@gmail.com>
2018-02-27 17:53:13 -0800

    Init script

---
+++ plot_shell.py
@@ -0,0 +1,27 @@
+import numpy as np
+import numba as nb
+import pandas as pd
+import pyarrow.parquet as pq 
+
+import matplotlib, os
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from pick_cloud_projection import pick_cid 
+import calc_radius
+
+# Given a field, return coarse observation
+def observe_coarse_field(Z, k):
+    S = np.add.reduceat(
+        np.add.reduceat(Z, np.arange(0, Z.shape[0], k), axis=0),
+                           np.arange(0, Z.shape[1], k), axis=1)
+
+    # Normalize coarse observation
+    S[S > 0] = 1
+    return S
+
+if __name__ == '__main__':
+    # Radius estimates 
+    r_g = calc_radius.calculate_radial_distance(df)
+    r_d = calc_radius.calculate_geometric_r(df)
