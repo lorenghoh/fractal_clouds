3854a85829d82c877d2ddc14d3f31bb2b35f65fb
Loren Oh <loren.gh.oh@gmail.com>
2018-02-27 16:03:36 -0800

    Repeat for multiple values of L/R

--- calc_pdim.py
+++ calc_pdim.py
@@ -22,30 +22,24 @@ def observe_coarse_field(Z, k):
     return S
 
 # Given a field, calculate perimeter
-def calc_perimeter(df):
-    return np.sum(Z[:, 1:] != Z[:, :-1]) + 
+def calc_perimeter(Z):
+    return np.sum(Z[:, 1:] != Z[:, :-1]) + \
            np.sum(Z[1:, :] != Z[:-1, :])
 
 if __name__ == '__main__':
     # Calculate fdim from a sample cloud 
     # Read horizontal slice from a cloud core
-    xy_map, x, y = pick_cid(4563, 0)
+    df = pick_cid(4563, 0)
 
-    x_width = max(x) - min(x)
-    y_width = max(y) - min(y)
-    print(f"\nCorresponding sub-domain size: {y_width}x{x_width}")
-    print( "Adjusted coordinates: " \
-          f"({min(y)}, {max(y)}), ({min(x)}, {max(x)})")
-
-    # Map the projection onto a new 2D array (2x size)
-    Z = np.zeros((y_width+4, x_width+4), dtype=int)
-    x = x - min(x) + 1
-    y = y - min(y) + 1
-    Z[y, x] = 1
+    x_width = max(df.x) - min(df.x)
+    y_width = max(df.y) - min(df.y)
+    xy_map = np.zeros((y_width+4, x_width+4), dtype=int)
+    xy_map[df.y, df.x] = 1
+    xy_map = observe_coarse_field(xy_map, 6)
 
     # Calculate perimeter and area
-    area = np.sum(Z[Z > 0])
-    p = calc_perimeter(Z)
+    area = np.sum(xy_map[xy_map > 0])
+    p = calc_perimeter(xy_map)
 
     print(p * 25, area * 25*25)
     print("Dp =", 2*np.log10(p*25)/np.log10(area*25*25))
