254d5678c5246bfe18e70b82b114e2537f73e519
Loren Oh <loren.gh.oh@gmail.com>
2018-02-28 01:27:55 -0800

    P as a function of varying L/R

--- calc_pdim.py
+++ calc_pdim.py
@@ -41,6 +41,12 @@ if __name__ == '__main__':
     r_d = calc_radius.calculate_geometric_r(df)
     sizes = np.arange(int(r_g), 0, -1)
 
+    # Calculate perimeter and area
+    area = np.sum(xy_map[xy_map > 0]) * 25**2
+    p = calc_perimeter(xy_map) * 25
+
+    print("Dp =", 2*np.log10(p)/np.log10(area))
+
     Dp = []
     for size in sizes:
         # Coefficient for horizontal scale
@@ -51,17 +57,45 @@ if __name__ == '__main__':
         p = calc_perimeter(Z) * C
 
         Dp.append(p)
-    sizes = sizes 
-
-    print(sizes, Dp)
+    sizes = sizes / r_g
 
     # Fit the successive log(sizes) with log (counts)
-    c = np.polyfit(np.log(sizes), np.log(Dp), 1)
+    c = np.polyfit(np.log10(sizes), np.log10(Dp), 1)
     print(-c[0])
 
-    # Calculate perimeter and area
-    area = np.sum(xy_map[xy_map > 0]) * 25**2
-    p = calc_perimeter(xy_map) * 25
+    #---- Plotting 
+    fig = plt.figure(1, figsize=(3, 3))
+    fig.clf()
+    sns.set_context('paper')
+    sns.set_style('ticks', 
+        {
+            'axes.grid': False, 
+            'axes.linewidth': '0.75',
+            'grid.color': '0.75',
+            'grid.linestyle': u':',
+            'legend.frameon': False,
+        })
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='Helvetica')
 
-    print(p, area)
-    print("Dp =", 2*np.log10(p)/np.log10(area))
+    cmap = sns.cubehelix_palette(start=1.2, hue=1, \
+                                 light=1, rot=-1.05, as_cmap=True)
+
+    ax = plt.subplot(1, 1, 1)
+    plt.xlabel(r'$\log_{10}$ $L/R$')
+    plt.ylabel(r'$\log_{10}$ $P$')
+
+    xi = np.linspace(min(np.log10(sizes)), max(np.log10(sizes)), 50)
+    plt.plot(xi, c[0]*xi+c[1], 'k-', lw=0.9)
+
+    label = f"P = (L/R)$^{{{-c[0]:.3f}}}$"
+    plt.plot(np.log10(sizes), np.log10(Dp), 
+             marker='o', lw=0.75, label=label)
+
+    plt.legend()
+
+    plt.tight_layout(pad=0.5)
+    figfile = 'png/{}.png'.format(os.path.splitext(__file__)[0])
+    print('\t Writing figure to {}...'.format(figfile))
+    plt.savefig(figfile,bbox_inches='tight', dpi=300, \
+                facecolor='w', transparent=True)
