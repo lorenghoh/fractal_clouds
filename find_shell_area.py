ef3526f2b83f7eaa91b457b4de7ff67878c1e9b5
Loren Oh <loren.gh.oh@gmail.com>
2018-02-28 15:33:56 -0800

    Calc change in shell area for increasing L/R

--- find_shell_area.py
+++ find_shell_area.py
@@ -10,6 +10,7 @@ import seaborn as sns
 
 from pick_cloud_projection import pick_cid 
 import calc_radius
+import calc_pdim
 
 # Given a field, return coarse observation
 def observe_coarse_field(Z, k):
@@ -18,7 +19,9 @@ def observe_coarse_field(Z, k):
                            np.arange(0, Z.shape[1], k), axis=1)
 
     # Normalize coarse observation
-    S[S > 0] = 1
+    thold = k**2 * 0.95
+    S[S < thold] = 0
+    S[S >= thold] = 1
     return S
 
 if __name__ == '__main__':
@@ -32,10 +35,52 @@ if __name__ == '__main__':
     # Radius estimates 
     r_g = calc_radius.calculate_radial_distance(df)
     r_d = calc_radius.calculate_geometric_r(df)
-    sizes = np.arange(int(r_g), 1, -1)
+    sizes = np.arange(int(r_g), 0, -1)
 
-    area_list = []
+    # Resolution
+    dx = 25
+
+    size, area = dx, np.array(np.sum(xy_map) * dx**2)
+
+    areas = []
     for size in sizes:
+        dxx = dx * size
         Z = observe_coarse_field(xy_map, size)
-        area_list.append(np.sum(Z))
-    print(area_list[-1] - area_list)
+        areas.append(np.array(area - np.sum(Z) * dxx**2)/area)
+
+    sizes = np.array(sizes)
+    # c = np.polyfit(sizes[:-1], areas[:-1], 1)
+    # print(c)
+
+    #---- Plotting 
+    fig = plt.figure(1, figsize=(3, 3))
+    fig.clf()
+    sns.set_context('paper')
+    sns.set_style('ticks', 
+        {
+            'axes.grid': False, 
+            'axes.linewidth': '0.75',
+            'grid.color': '0.75',
+            'grid.linestyle': u':',
+            'legend.frameon': False,
+        })
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='Helvetica')
+
+    cmap = sns.cubehelix_palette(start=1.2, hue=1, \
+                                 light=1, rot=-1.05, as_cmap=True)
+
+    ax = plt.subplot(1, 1, 1)
+    plt.xlabel(r'$S$')
+    plt.ylabel(r'$\mathcal{A}_\mathrm{s}/\mathcal{A}$')
+
+    # xi = np.linspace(min(sizes), max(sizes), 50)
+    # plt.plot(xi, c[0]*xi+c[1], 'k-', lw=0.9)
+
+    plt.plot(sizes, areas, marker='o', lw=0.75)
+
+    plt.tight_layout(pad=0.5)
+    figfile = 'png/{}.png'.format(os.path.splitext(__file__)[0])
+    print('\t Writing figure to {}...'.format(figfile))
+    plt.savefig(figfile,bbox_inches='tight', dpi=300, \
+                facecolor='w', transparent=True)
