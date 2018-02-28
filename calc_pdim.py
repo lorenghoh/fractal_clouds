fc1731577b722823028b7cf16b00168602ab23d7
Loren Oh <loren.gh.oh@gmail.com>
2018-02-27 16:00:31 -0800

    Calculate perimeter for coarse observation

--- calc_pdim.py
+++ calc_pdim.py
@@ -9,6 +9,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from pick_cloud_projection import pick_cid 
+import calc_radius
+
+# Given a field, return coarse observation
+def observe_coarse_field(Z, k):
+    S = np.add.reduceat(
+        np.add.reduceat(Z, np.arange(0, Z.shape[0], k), axis=0),
+                           np.arange(0, Z.shape[1], k), axis=1)
+
+    # Normalize coarse observation
+    S[S > 0] = 1
+    return S
+
+# Given a field, calculate perimeter
+def calc_perimeter(df):
+    return np.sum(Z[:, 1:] != Z[:, :-1]) + 
+           np.sum(Z[1:, :] != Z[:-1, :])
 
 if __name__ == '__main__':
     # Calculate fdim from a sample cloud 
@@ -26,11 +42,10 @@ if __name__ == '__main__':
     x = x - min(x) + 1
     y = y - min(y) + 1
     Z[y, x] = 1
-    Z[Z > 0] = 1
 
     # Calculate perimeter and area
     area = np.sum(Z[Z > 0])
-    p = np.sum(Z[:, 1:] != Z[:, :-1]) + np.sum(Z[1:, :] != Z[:-1, :])
+    p = calc_perimeter(Z)
 
     print(p * 25, area * 25*25)
     print("Dp =", 2*np.log10(p*25)/np.log10(area*25*25))
