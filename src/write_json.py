a886698bbaeb4116b545e108a34f97d4edf329f4
Loren Oh <loren.gh.oh@gmail.com>
2018-07-14 16:09:34 -0700

    Write case config in json

--- src/write_json.py
+++ src/write_json.py
@@ -1,171 +1,288 @@
-from collections import OrderedDict
-import json, pprint
+import sys, json, pprint
 
+def main(case_name):
+    json_dict = {}
 
-def main():
-    json_dict = OrderedDict()
+    if case_name == 'BOMEX':
+        #---- BOMEX 
+        json_dict = {}
+        json_dict['config'] = {}
 
-    #---- BOMEX 
-    json_dict['BOMEX'] = OrderedDict()
-    json_dict['BOMEX']['config'] = OrderedDict()
+        stat_file = 'BOMEX_256x256x128_25m_25m_1s_stat.nc'
+        location = '/newtera/loh/data/BOMEX'
 
-    json_dict['BOMEX']['location'] = '/newtera/loh/data/BOMEX'
+        json_dict['case_name'] = case_name
+        json_dict['location'] = location
 
-    stat_file = 'BOMEX_256x256x128_25m_25m_1s_stat.nc'
+        json_dict['condensed'] = '%s/condensed_entrain' % location
+        json_dict['core'] = '%s/core_entrain' % location
+        json_dict['stat_file'] = '%s/%s' % (location, stat_file)
+        json_dict['tracking'] = '%s/tracking' % location
+        json_dict['variables'] = '%s/variables' % location
 
-    json_dict['BOMEX']['condensed'] = '%s/condensed_entrain' % json_dict['BOMEX']['location']
-    json_dict['BOMEX']['core'] = '%s/core_entrain' % json_dict['BOMEX']['location']
-    json_dict['BOMEX']['stat_file'] = '%s/%s' % (json_dict['BOMEX']['location'], stat_file)
-    json_dict['BOMEX']['tracking'] = '%s/tracking' % json_dict['BOMEX']['location']
-    json_dict['BOMEX']['variables'] = '%s/variables' % json_dict['BOMEX']['location']
+        # Model parameters
+        json_dict['config']['nx'] = 256
+        json_dict['config']['ny'] = 256
+        json_dict['config']['nz'] = 128
+        json_dict['config']['nt'] = 180
 
-    # Model parameters
-    json_dict['BOMEX']['config']['nx'] = 256
-    json_dict['BOMEX']['config']['ny'] = 256
-    json_dict['BOMEX']['config']['nz'] = 128
-    json_dict['BOMEX']['config']['nt'] = 180
+        json_dict['config']['dx'] = 25
+        json_dict['config']['dy'] = 25
+        json_dict['config']['dz'] = 25
+        json_dict['config']['dt'] = 60
 
-    json_dict['BOMEX']['config']['dx'] = 25
-    json_dict['BOMEX']['config']['dt'] = 60
+        json_dict['config']['ug'] = -8.
+        json_dict['config']['vg'] = 0.
 
-    json_dict['BOMEX']['config']['ug'] = -8.
-    json_dict['BOMEX']['config']['vg'] = 0.
+    elif case_name == 'BOMEX_HRES':
+        #---- BOMEX 12.5-m Resolution
+        json_dict = {}
+        json_dict['config'] = {}
 
+        stat_file = 'BOMEX_512x512x256_12.5m_12.5m_1s_ent.nc'
+        location = '/Howard16TB/data/loh/BOMEX_HRES'
 
-    #---- CGILS_300K
-    json_dict['CGILS_300K'] = OrderedDict()
-    json_dict['CGILS_300K']['config'] = OrderedDict()
+        json_dict['case_name'] = case_name
+        json_dict['location'] = location
 
-    json_dict['CGILS_300K']['location'] = '/newtera/loh/data/CGILS_300K'
+        json_dict['condensed'] = '%s/condensed_entrain' % location
+        json_dict['core'] = '%s/core_entrain' % location
+        json_dict['stat_file'] = '%s/%s' % (location, stat_file)
+        json_dict['variables'] = '%s/variables' % location
+        json_dict['tracking'] = '/nodessd/loh/tracking/BOMEX_HRES'
 
-    stat_file = 'ENT_CGILS_S6_IDEAL_3D_SST_300K_384x384x194_25m_1s_stat.nc'
-    
-    json_dict['CGILS_300K']['condensed'] = '%s/condensed_entrain' % json_dict['CGILS_300K']['location']
-    json_dict['CGILS_300K']['core'] = '%s/core_entrain' % json_dict['CGILS_300K']['location']
-    json_dict['CGILS_300K']['stat_file'] = '%s/%s' % (json_dict['CGILS_300K']['location'], stat_file)
-    json_dict['CGILS_300K']['tracking'] = '%s/tracking' % json_dict['CGILS_300K']['location']
-    json_dict['CGILS_300K']['variables'] = '%s/variables' % json_dict['CGILS_300K']['location']
+        # Model parameters
+        json_dict['config']['nx'] = 512
+        json_dict['config']['ny'] = 512
+        json_dict['config']['nz'] = 256
+        json_dict['config']['nt'] = 180
 
-    # Model parameters
-    json_dict['CGILS_300K']['config']['nt'] = 360
-    json_dict['CGILS_300K']['config']['nx'] = 384
-    json_dict['CGILS_300K']['config']['ny'] = 384
-    json_dict['CGILS_300K']['config']['nz'] = 194
+        json_dict['config']['dx'] = 12.5
+        json_dict['config']['dy'] = 12.5
+        json_dict['config']['dz'] = 12.5
+        json_dict['config']['dt'] = 60
 
-    json_dict['CGILS_300K']['config']['dx'] = 25
-    json_dict['CGILS_300K']['config']['dt'] = 60
+        json_dict['config']['ug'] = -8.
+        json_dict['config']['vg'] = 0.
 
-    json_dict['CGILS_300K']['config']['ug'] = 0.
-    json_dict['CGILS_300K']['config']['vg'] = 0.
+    elif(case_name == 'BOMEX_12HR'):
+        #---- BOMEX 12-hour Run
+        json_dict = {}
+        json_dict['config'] = {}
 
+        stat_file = 'BOMEX_512x512x256_12.5m_12.5m_1s_ent.nc'
+        location = '/Howard16TB/data/loh/BOMEX_12HR'
 
-    #---- CGILS_301K
-    json_dict['CGILS_301K'] = OrderedDict()
-    json_dict['CGILS_301K']['config'] = OrderedDict()
+        json_dict['case_name'] = case_name
+        json_dict['location'] = location
 
-    json_dict['CGILS_301K']['location'] = '/newtera/loh/data/CGILS_301K'
+        json_dict['condensed'] = '%s/condensed_entrain' % location
+        json_dict['core'] = '%s/core_entrain' % location
+        json_dict['stat_file'] = '%s/%s' % (location, stat_file)
+        json_dict['variables'] = '%s/variables' % location
+        json_dict['tracking'] = '/nodessd/loh/tracking/BOMEX_12HR'
 
-    stat_file = 'ENT_CGILS_S6_IDEAL_3D_SST_301K_384x384x194_25m_1s_stat.nc'
-    
-    json_dict['CGILS_301K']['condensed'] = '%s/condensed_entrain' % json_dict['CGILS_301K']['location']
-    json_dict['CGILS_301K']['core'] = '%s/core_entrain' % json_dict['CGILS_301K']['location']
-    json_dict['CGILS_301K']['stat_file'] = '%s/%s' % (json_dict['CGILS_301K']['location'], stat_file)
-    json_dict['CGILS_301K']['tracking'] = '%s/tracking' % json_dict['CGILS_301K']['location']
-    json_dict['CGILS_301K']['variables'] = '%s/variables' % json_dict['CGILS_301K']['location']
+        # Model parameters
+        json_dict['config']['nx'] = 512
+        json_dict['config']['ny'] = 512
+        json_dict['config']['nz'] = 128
+        json_dict['config']['nt'] = 540
 
-    # Model parameters
-    json_dict['CGILS_301K']['config']['nt'] = 360
-    json_dict['CGILS_301K']['config']['nx'] = 384
-    json_dict['CGILS_301K']['config']['ny'] = 384
-    json_dict['CGILS_301K']['config']['nz'] = 194
+        json_dict['config']['dx'] = 25
+        json_dict['config']['dy'] = 25
+        json_dict['config']['dz'] = 25
+        json_dict['config']['dt'] = 60
 
-    json_dict['CGILS_301K']['config']['dx'] = 25
-    json_dict['CGILS_301K']['config']['dt'] = 60
+        json_dict['config']['ug'] = -8.
+        json_dict['config']['vg'] = 0.
 
-    json_dict['CGILS_301K']['config']['ug'] = 0.
-    json_dict['CGILS_301K']['config']['vg'] = 0.
+    elif case_name == 'CGILS_CTL':
+        #---- CGILS_CTL
+        json_dict = {}
+        json_dict['config'] = {}
 
+        stat_file = 'ENT_CGILS_CTL_S6_3D_384x384x194_25m_1s_stat.nc'
+        location = '/newtera/loh/data/CGILS_CTL'
 
-    #---- GCSSARM
-    json_dict['GCSSARM'] = OrderedDict()
-    json_dict['GCSSARM']['config'] = OrderedDict()
+        json_dict['case_name'] = case_name
+        json_dict['location'] = location
+        
+        json_dict['condensed'] = '%s/condensed_entrain' % location
+        json_dict['core'] = '%s/core_entrain' % location
+        json_dict['stat_file'] = '%s/%s' % (location, stat_file)
+        json_dict['tracking'] = '%s/tracking' % location
+        json_dict['variables'] = '%s/variables' % location
 
-    json_dict['GCSSARM']['location'] = '/newtera/loh/data/GCSSARM'
+        # Model parameters
+        json_dict['config']['nt'] = 360
+        json_dict['config']['nx'] = 384
+        json_dict['config']['ny'] = 384
+        json_dict['config']['nz'] = 194
 
-    stat_file = 'GCSSARM_256x256x208_25m_25m_1s_stat.nc'
+        json_dict['config']['dx'] = 25
+        json_dict['config']['dy'] = 25
+        json_dict['config']['dz'] = 25
+        json_dict['config']['dt'] = 60
 
-    json_dict['GCSSARM']['condensed'] = '%s/condensed_entrain' % json_dict['GCSSARM']['location']
-    json_dict['GCSSARM']['core'] = '%s/core_entrain' % json_dict['GCSSARM']['location']
-    json_dict['GCSSARM']['stat_file'] = '%s/%s' % (json_dict['GCSSARM']['location'], stat_file)
-    json_dict['GCSSARM']['tracking'] = '%s/tracking' % json_dict['GCSSARM']['location']
-    json_dict['GCSSARM']['variables'] = '%s/variables' % json_dict['GCSSARM']['location']
+        json_dict['config']['ug'] = 0.
+        json_dict['config']['vg'] = 0.
 
-    # Model parameters
-    json_dict['GCSSARM']['config']['nx'] = 256
-    json_dict['GCSSARM']['config']['ny'] = 256
-    json_dict['GCSSARM']['config']['nz'] = 128
-    json_dict['GCSSARM']['config']['nt'] = 510
+    elif case_name == 'CGILS_300K':
+        #---- CGILS_300K
+        json_dict = {}
+        json_dict['config'] = {}
 
-    json_dict['GCSSARM']['config']['dx'] = 25
-    json_dict['GCSSARM']['config']['dt'] = 60
+        stat_file = 'ENT_CGILS_S6_IDEAL_3D_SST_300K_384x384x194_25m_1s_stat.nc'
+        location = '/newtera/loh/data/CGILS_300K'
 
-    json_dict['GCSSARM']['config']['ug'] = 10.
-    json_dict['GCSSARM']['config']['vg'] = 0.
+        json_dict['case_name'] = case_name
+        json_dict['location'] = location
+        
+        json_dict['condensed'] = '%s/condensed_entrain' % location
+        json_dict['core'] = '%s/core_entrain' % location
+        json_dict['stat_file'] = '%s/%s' % (location, stat_file)
+        json_dict['tracking'] = '%s/tracking' % location
+        json_dict['variables'] = '%s/variables' % location
 
+        # Model parameters
+        json_dict['config']['nt'] = 360
+        json_dict['config']['nx'] = 384
+        json_dict['config']['ny'] = 384
+        json_dict['config']['nz'] = 194
 
-    #---- GATE_BDL
-    json_dict['GATE_BDL'] = OrderedDict()
-    json_dict['GATE_BDL']['config'] = OrderedDict()
+        json_dict['config']['dx'] = 25
+        json_dict['config']['dy'] = 25
+        json_dict['config']['dz'] = 25
+        json_dict['config']['dt'] = 60
 
-    json_dict['GATE_BDL']['location'] = '/newtera/loh/data/GATE_BDL'
+        json_dict['config']['ug'] = 0.
+        json_dict['config']['vg'] = 0.
 
-    stat_file = 'GATE_1920x1920x512_50m_1s_ent_stat.nc'
+    elif case_name == 'CGILS_301K':
+        #---- CGILS_301K
+        json_dict = {}
+        json_dict['config'] = {}
 
-    json_dict['GATE_BDL']['condensed'] = '%s/condensed_entrain' % json_dict['GATE_BDL']['location']
-    json_dict['GATE_BDL']['core'] = '%s/core_entrain' % json_dict['GATE_BDL']['location']
-    json_dict['GATE_BDL']['stat_file'] = '%s/%s' % (json_dict['GATE_BDL']['location'], stat_file)
-    json_dict['GATE_BDL']['tracking'] = '%s/tracking' % json_dict['GATE_BDL']['location']
-    json_dict['GATE_BDL']['variables'] = '%s/variables' % json_dict['GATE_BDL']['location']
+        stat_file = 'ENT_CGILS_S6_IDEAL_3D_SST_301K_384x384x194_25m_1s_stat.nc'
+        location = '/newtera/loh/data/CGILS_301K'
 
-    # Model parameters
-    json_dict['GATE_BDL']['config']['nx'] = 1728
-    json_dict['GATE_BDL']['config']['ny'] = 1728
-    json_dict['GATE_BDL']['config']['nz'] = 80
-    json_dict['GATE_BDL']['config']['nt'] = 180
+        json_dict['case_name'] = case_name
+        json_dict['location'] = location
+        
+        json_dict['condensed'] = '%s/condensed_entrain' % location
+        json_dict['core'] = '%s/core_entrain' % location
+        json_dict['stat_file'] = '%s/%s' % (location, stat_file)
+        json_dict['tracking'] = '%s/tracking' % location
+        json_dict['variables'] = '%s/variables' % location
 
-    json_dict['GATE_BDL']['config']['dx'] = 50
-    json_dict['GATE_BDL']['config']['dt'] = 60
+        # Model parameters
+        json_dict['config']['nt'] = 360
+        json_dict['config']['nx'] = 384
+        json_dict['config']['ny'] = 384
+        json_dict['config']['nz'] = 194
 
-    json_dict['GATE_BDL']['config']['ug'] = -8.
-    json_dict['GATE_BDL']['config']['vg'] = 0.
+        json_dict['config']['dx'] = 25
+        json_dict['config']['dy'] = 25
+        json_dict['config']['dz'] = 25
+        json_dict['config']['dt'] = 60
 
+        json_dict['config']['ug'] = 0.
+        json_dict['config']['vg'] = 0.
 
-    #---- GATE
-    json_dict['GATE'] = OrderedDict()
-    json_dict['GATE']['config'] = OrderedDict()
+    elif case_name == 'GCSSARM':
+        #---- GCSSARM
+        json_dict = {}
+        json_dict['config'] = {}
 
-    json_dict['GATE']['location'] = '/newtera/loh/data/GATE'
+        stat_file = 'GCSSARM_256x256x208_25m_25m_1s_stat.nc'
+        location = '/newtera/loh/data/GCSSARM'
 
-    stat_file = 'GATE_1920x1920x512_50m_1s_ent_stat.nc'
+        json_dict['case_name'] = case_name
+        json_dict['location'] = location
 
-    json_dict['GATE']['condensed'] = '%s/condensed_entrain' % json_dict['GATE']['location']
-    json_dict['GATE']['core'] = '%s/core_entrain' % json_dict['GATE']['location']
-    json_dict['GATE']['stat_file'] = '%s/%s' % (json_dict['GATE']['location'], stat_file)
-    json_dict['GATE']['tracking'] = '/tera/loh/cloudtracker/cloudtracker/hdf5'
-    # json_dict['GATE']['tracking'] = '%s/tracking' % json_dict['GATE']['location']
-    json_dict['GATE']['variables'] = '%s/variables' % json_dict['GATE']['location']
+        json_dict['condensed'] = '%s/condensed_entrain' % location
+        json_dict['core'] = '%s/core_entrain' % location
+        json_dict['stat_file'] = '%s/%s' % (location, stat_file)
+        json_dict['tracking'] = '%s/tracking' % location
+        json_dict['variables'] = '%s/variables' % location
 
-    # Model parameters
-    json_dict['GATE']['config']['nx'] = 1728
-    json_dict['GATE']['config']['ny'] = 1728
-    json_dict['GATE']['config']['nz'] = 320
-    json_dict['GATE']['config']['nt'] = 30
+        # Model parameters
+        json_dict['config']['nx'] = 256
+        json_dict['config']['ny'] = 256
+        json_dict['config']['nz'] = 128
+        json_dict['config']['nt'] = 510
 
-    json_dict['GATE']['config']['dx'] = 50
-    json_dict['GATE']['config']['dt'] = 60
+        json_dict['config']['dx'] = 25
+        json_dict['config']['dy'] = 25
+        json_dict['config']['dz'] = 25
+        json_dict['config']['dt'] = 60
 
-    json_dict['GATE']['config']['ug'] = -8.
-    json_dict['GATE']['config']['vg'] = 0.
+        json_dict['config']['ug'] = 10.
+        json_dict['config']['vg'] = 0.
+
+    elif case_name == 'GATE_BDL':
+        #---- GATE_BDL
+        json_dict = {}
+        json_dict['config'] = {}
+
+        stat_file = 'GATE_1920x1920x512_50m_1s_ent_stat.nc'
+        location = '/newtera/loh/data/GATE_BDL'
+
+        json_dict['case_name'] = case_name
+        json_dict['location'] = location
+
+        json_dict['condensed'] = '%s/condensed_entrain' % location
+        json_dict['core'] = '%s/core_entrain' % location
+        json_dict['stat_file'] = '%s/%s' % (location, stat_file)
+        json_dict['tracking'] = '%s/tracking' % location
+        json_dict['variables'] = '%s/variables' % location
+
+        # Model parameters
+        json_dict['config']['nx'] = 1728
+        json_dict['config']['ny'] = 1728
+        json_dict['config']['nz'] = 80
+        json_dict['config']['nt'] = 180
+
+        json_dict['config']['dx'] = 50
+        json_dict['config']['dy'] = 50
+        json_dict['config']['dz'] = 50
+        json_dict['config']['dt'] = 60
+
+        json_dict['config']['ug'] = -8.
+        json_dict['config']['vg'] = 0.
+
+    elif case_name == 'GATE':
+        #---- GATE
+        json_dict = {}
+        json_dict['config'] = {}
+
+        stat_file = 'GATE_1920x1920x512_50m_1s_ent_stat.nc'
+        location = '/newtera/loh/data/GATE'
+
+        json_dict['case_name'] = case_name
+        json_dict['location'] = location
+
+        json_dict['condensed'] = '%s/condensed_entrain' % location
+        json_dict['core'] = '%s/core_entrain' % location
+        json_dict['stat_file'] = '%s/%s' % (location, stat_file)
+        json_dict['tracking'] = '/tera/loh/cloudtracker/cloudtracker/hdf5'
+        json_dict['variables'] = '%s/variables' % location
+
+        # Model parameters
+        json_dict['config']['nx'] = 1728
+        json_dict['config']['ny'] = 1728
+        json_dict['config']['nz'] = 320
+        json_dict['config']['nt'] = 30
+
+        json_dict['config']['dx'] = 50
+        json_dict['config']['dy'] = 50
+        json_dict['config']['dz'] = 50
+        json_dict['config']['dt'] = 60
+
+        json_dict['config']['ug'] = -8.
+        json_dict['config']['vg'] = 0.
+    else:
+        raise ValueError('Case name not found')
 
 
     with open('model_config.json','w') as f:
@@ -175,4 +292,9 @@ def main():
         pp.pprint(json_dict)
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 2:
+        main(sys.argv[-1])
+    else:
+        print("Missing parameter")
+        print("For example, run python write_json.py BOMEX \n")
+        raise ValueError('Case name is not given')
