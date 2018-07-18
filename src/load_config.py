b4a11334886127f5280198b71b855b64b6190d73
Loren Oh <loren.gh.oh@gmail.com>
2018-07-18 16:01:05 -0700

    Module loading model configurations

---
+++ src/load_config.py
@@ -0,0 +1,10 @@
+import ujson as json
+
+with open('model_config.json', 'r') as file:
+    try:
+        config = json.load(file)
+    except:
+        raise("JSON config not found")
+    
+    c = config['config']
+    del config['config']
