376d6d972ea909549a22511cca834b4c031e1448
Loren Oh <loren.gh.oh@gmail.com>
2018-07-18 16:05:46 -0700

    Load config from module

--- src/find_largest_clouds.py
+++ src/find_largest_clouds.py
@@ -1,12 +1,14 @@
 import pandas as pd
-import pyarrow.parquet as pq 
+import pyarrow.parquet as pq
+
+from load_config import config
 
 def find_largest_clouds(filename):
     df = pq.read_pandas(filename).to_pandas()
     return df.cid.value_counts()
 
 if __name__ == '__main__':
-    filename = 'tracking/clouds_00000121.pq'
+    filename = f'{config["tracking"]}/clouds_00000121.pq'
     counts = find_largest_clouds(filename)
 
     print(counts[:10])
