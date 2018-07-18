6b27b2a8a432be547049a1d863059512e016fe15
Loren Oh <loren.gh.oh@gmail.com>
2018-07-18 16:12:55 -0700

    Implement attrdict

--- src/load_config.py
+++ src/load_config.py
@@ -1,12 +1,17 @@
 import ujson as json
 
+class attrdict(dict):
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.__dict__ = self
+
 with open('model_config.json', 'r') as file:
     try:
-        config = json.load(file)
+        config = attrdict(json.load(file))
     except:
         raise("JSON config not found")
     
-    c = config['config']
+    c = attrdict(config['config'])
     del config['config']
 
 if __name__ == '__main__':
