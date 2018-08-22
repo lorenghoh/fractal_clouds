a4c9828f429ed0eb9299f32372e0a9ad2e97a651
Loren Oh <loh@eoas.ubc.ca>
2018-08-22 15:40:30 -0700

    Replace cloud + fix axis labels

--- src/plot_observed_cloud.py
+++ src/plot_observed_cloud.py
@@ -14,7 +14,7 @@ import calc_radius
 if __name__ == '__main__':
     # Calculate fdim from a sample cloud 
     # Read horizontal slice from a cloud core
-    df = pick_cid(4563, 0)
+    df = pick_cid(3621, 0)
 
     x_width = max(df.x) - min(df.x)
     y_width = max(df.y) - min(df.y)
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     xy_map[df.y, df.x] = 1
 
     #---- Plotting 
-    fig = plt.figure(1, figsize=(5, 4))
+    fig = plt.figure(1, figsize=(5, 5))
     fig.clf()
     sns.set_context('paper')
     sns.set_style('ticks', 
@@ -34,7 +34,7 @@ if __name__ == '__main__':
             'legend.frameon': True,
         })
     plt.rc('text', usetex=True)
-    plt.rc('font', family='Helvetica')
+    plt.rc('font', family='Serif')
 
     cmap = sns.cubehelix_palette(start=1.2, hue=1, \
                                  light=1, rot=-1.05, as_cmap=True)
@@ -44,28 +44,33 @@ if __name__ == '__main__':
     yi = np.arange(y_width+4)
 
     x_com, y_com = calc_radius.calculate_com(df)
-    r_g = calc_radius.calculate_radial_distance(df)
-    r_d = calc_radius.calculate_geometric_r(df)
+    r_d = calc_radius.calculate_radial_distance(df)
+    r_g = calc_radius.calculate_geometric_r(df)
 
     C1 = sns.crayons['Mahogany']
     C2 = sns.crayons['Denim']
-    c1 = plt.Circle((x_com, y_com), r_g, color=C1, fill=False, lw=1.25)
-    c2 = plt.Circle((x_com, y_com), r_d, color=C2, fill=False, lw=2)
+    c1 = plt.Circle((x_com, y_com), r_d, color=C1, fill=False, lw=1.25)
+    c2 = plt.Circle((x_com, y_com), r_g, color=C2, fill=False, lw=2)
     ax.add_artist(c1)
     ax.add_artist(c2)
 
-    plt.plot([x_com, x_com+r_g*np.sqrt(2)/2], \
-             [y_com, y_com+r_g/np.sqrt(2)], color=C1, \
-             lw=1.25, label=rf"$r_g$ $\sim$ {r_g*25:.0f} m")
-    plt.plot([x_com, x_com-r_d*np.sqrt(2)/2], 
-             [y_com, y_com+r_d/np.sqrt(2)], color=C2, \
-             lw=2, label=rf"$r_d$ $\sim$ {r_d*25:.0f} m")
-    plt.legend(fontsize=10, loc=2)
+    plt.plot([x_com, x_com+r_d*np.sqrt(2)/2], \
+             [y_com, y_com+r_d/np.sqrt(2)], color=C1, \
+             lw=1.25, label=rf"Ave. Rad. Distance $r_d$ $\sim$ {r_d*25:.0f} m")
+    plt.plot([x_com, x_com-r_g*np.sqrt(2)/2], 
+             [y_com, y_com+r_g/np.sqrt(2)], color=C2, \
+             lw=2, label=rf"Geometric Radius $r_g$ $\sim$ {r_g*25:.0f} m")
+    plt.legend(fontsize=12, loc=2)
 
     im = plt.pcolormesh(xi, yi, xy_map, cmap=cmap, lw=0.5)
 
+    plt.xticks(xi[::5], xi[::5] * 25 / 1e3)
+    plt.yticks(yi[::5], yi[::5] * 25 / 1e3)
+    plt.xlabel('x [km]')
+    plt.ylabel('y [km]')
+
     plt.tight_layout(pad=0.5)
-    figfile = 'png/{}.png'.format(os.path.splitext(__file__)[0])
+    figfile = '../png/{}.png'.format(os.path.splitext(__file__)[0])
     print('\t Writing figure to {}...'.format(figfile))
     plt.savefig(figfile,bbox_inches='tight', dpi=180, \
                 facecolor='w', transparent=True)
