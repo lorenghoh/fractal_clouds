5f9fba5cdfdb728b0a9fa61c26ecc17caf5c34dc
Loren Oh <loh@eoas.ubc.ca>
2018-08-23 16:25:18 -0700

    Axis labelling

--- src/plot_shell.py
+++ src/plot_shell.py
@@ -19,13 +19,13 @@ def observe_coarse_field(Z, k):
                            np.arange(0, Z.shape[1], k), axis=1)
 
     # Normalize coarse observation
-    thold = k**2 * 0.95
-    S[S < thold] = 0
-    S[S >= thold] = 1
+    thold = k**2 * 0.9
+    S[S <= thold] = 0
+    S[S > thold] = 1
     return S
 
 if __name__ == '__main__':
-    df = pick_cid(4563, 0)
+    df = pick_cid(35145, 0)
 
     x_width = max(df.x) - min(df.x)
     y_width = max(df.y) - min(df.y)
@@ -33,9 +33,9 @@ if __name__ == '__main__':
     xy_map[df.y, df.x] = 1
 
     # Radius estimates 
-    r_g = calc_radius.calculate_radial_distance(df)
-    r_d = calc_radius.calculate_geometric_r(df)
-    sizes = np.arange(int(r_g), 0, -1)
+    # r_ = calc_radius.calculate_radial_distance(df)
+    r_ = calc_radius.calculate_geometric_r(df)
+    sizes = np.arange(int(r_), 0, -1)
 
     # Resolution
     dx = 25
@@ -48,9 +48,7 @@ if __name__ == '__main__':
         Z = observe_coarse_field(xy_map, size)
         areas.append(np.array(area - np.sum(Z) * dxx**2)/area)
 
-    sizes = np.array(sizes)
-    # c = np.polyfit(sizes[:-1], areas[:-1], 1)
-    # print(c)
+    sizes = sizes / r_
 
     #---- Plotting 
     fig = plt.figure(1, figsize=(3, 3))
@@ -65,22 +63,19 @@ if __name__ == '__main__':
             'legend.frameon': False,
         })
     plt.rc('text', usetex=True)
-    plt.rc('font', family='Helvetica')
+    plt.rc('font', family='Serif')
 
     cmap = sns.cubehelix_palette(start=1.2, hue=1, \
                                  light=1, rot=-1.05, as_cmap=True)
 
     ax = plt.subplot(1, 1, 1)
-    plt.xlabel(r'$S$')
-    plt.ylabel(r'$\mathcal{A}_\mathrm{s}/\mathcal{A}$')
-
-    # xi = np.linspace(min(sizes), max(sizes), 50)
-    # plt.plot(xi, c[0]*xi+c[1], 'k-', lw=0.9)
+    plt.xlabel(r'$S = L/R$')
+    plt.ylabel(r'Rel. Shell Region $\mathcal{A}_\mathrm{s}/\mathcal{A}$')
 
     plt.plot(sizes, areas, marker='o', lw=0.75)
 
     plt.tight_layout(pad=0.5)
-    figfile = 'png/{}.png'.format(os.path.splitext(__file__)[0])
+    figfile = '../png/{}.png'.format(os.path.splitext(__file__)[0])
     print('\t Writing figure to {}...'.format(figfile))
     plt.savefig(figfile,bbox_inches='tight', dpi=300, \
                 facecolor='w', transparent=True)
